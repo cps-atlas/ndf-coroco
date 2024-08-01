4c03edd417c0a7d185b192c4d88d8e0f9447f3a0
kehan <kehan.long@toyota.com>
2024-08-01 15:23:34 -0400

    update charging

--- robot_config.py
+++ robot_config.py
@@ -6,7 +6,7 @@ LINK_LENGTH = 2.0
 MIN_CABLE_LENGTH = LINK_LENGTH * 0.8
 MAX_CABLE_LENGTH = LINK_LENGTH * 1.2
 
-NUM_OF_LINKS = 4
+NUM_OF_LINKS = 5
 
 # Environment Configuration for the random dyanmical env with sphere obsatcles
 NUM_ENVIRONMENTS = 1
